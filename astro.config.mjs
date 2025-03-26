75be4be7679ef70b4f1109fc071a14d925667285
noahrognon <noah.rognon@edu.univ-fcomte.fr>
2025-03-26 16:25:51 +0100

    fin 26/03/25

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-    experimental: { svg: true },
+  experimental: { svg: true },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [alpinejs()]
 });
