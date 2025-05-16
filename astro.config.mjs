c4f77e6db159042f3f4dfff44145f3d89c6a80cc
noahrognon <noah.rognon@edu.univ-fcomte.fr>
2025-05-16 10:11:52 +0200

    deploiement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // astro.config.mjs
 
-
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import alpinejs from '@astrojs/alpinejs';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   experimental: { svg: true },
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [alpinejs()],
-  output: "server"
 });
