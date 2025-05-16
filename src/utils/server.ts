d46227ea0111682470e702d355e45784d8f20b2d
noahrognon <noah.rognon@edu.univ-fcomte.fr>
2025-05-16 13:49:15 +0200

    erreur deploiement

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,6 +1,6 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("https://emminent.noahrognon.fr:443");
+export const pb = new PocketBase("https://emminent.noahrognon.fr:443");
 
 export default async function getContext({ locals }) {
     locals.pb = pb;
